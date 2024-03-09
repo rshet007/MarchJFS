a558533096cbf6a89097c979da54d746e9c7037f
daneshwarsh <daneshwarrh@gmail.com>
2024-03-09 16:09:20 +0530

    12345

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String []args){
+        int a,b,c;
+        a=34;
+        b=324;
+        c=a+b;
+        system.out.println("sum: "+c);
+
+    }
+}
